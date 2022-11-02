d003ad645c0a7945c61c70f73d3456622b1211a0
Amila-Software-Engineer <amilasoften@gmail.com>
2022-11-02 20:57:38 +0530

    student module

--- src/main/java/com/example/day01project/Day01ProjectApplication.java
+++ src/main/java/com/example/day01project/Day01ProjectApplication.java
@@ -2,12 +2,20 @@ package com.example.day01project;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.List;
 
 @SpringBootApplication
+@RestController
 public class Day01ProjectApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Day01ProjectApplication.class, args);
 	}
+
 
 }
---
+++ src/main/java/com/example/day01project/student/StudentController.java
@@ -0,0 +1,31 @@
+package com.example.day01project.student;
+
+
+import com.example.day01project.student.Student;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.List;
+
+@RestController
+@RequestMapping("api/v1/students")
+public class StudentController {
+    private final StudentService studentService;
+
+    @Autowired
+    public StudentController(StudentService studentService) {
+        this.studentService = studentService;
+    }
+
+    @GetMapping
+    public List<Student> getStudents() {
+        return studentService.getStudents();
+    }
+
+    @PostMapping
+    public void registerNewStudent(@RequestBody Student student){
+        studentService.addNewStudent(student);
+    }
+}
