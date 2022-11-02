a1841d99a84c6c8ab6e783dc1c8dfa7761ae6079
Amila-Software-Engineer <amilasoften@gmail.com>
2022-11-02 10:29:08 +0530

    init

---
+++ src/main/java/com/example/day01project/Day01ProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.day01project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Day01ProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Day01ProjectApplication.class, args);
+	}
+
+}
